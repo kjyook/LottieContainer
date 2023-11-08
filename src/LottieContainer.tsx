141670d59d287b92ec039e5a040742bf2d31fdb5
kjyook <106497893+kjyook@users.noreply.github.com>
2023-11-08 18:29:57 +0900

    LottieContainer publishing

---
+++ src/LottieContainer.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react';
+import Lottie from 'lottie-web';
+import styled from '@emotion/styled';
+
+type LottieContainerProps = {
+    path: string;
+    width?: string;
+    height?: string;
+};
+
+const LottieContainer = ({ path, width, height }: LottieContainerProps) => {
+  const lottieContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+        Lottie.loadAnimation({
+            container: lottieContainer.current as HTMLDivElement,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            path: path,
+            });
+            return () => Lottie.destroy();
+    }, []);
+
+    const Root = styled.div`
+        width: ${width};
+        height: ${height};
+    `;
+
+    return (
+        <>
+            <Root ref={lottieContainer} />
+        </>
+    );
+};
+
+export default LottieContainer;
